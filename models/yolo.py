f8f31f6956c0feea98239ab323c2759f6eb5f282
Jan Jongboom <janjongboom@gmail.com>
2022-10-30 12:36:48 +0100

    Fix onnx export with --grid

--- models/yolo.py
+++ models/yolo.py
@@ -52,7 +52,7 @@ class Detect(nn.Module):
 
                 y = x[i].sigmoid()
                 y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * torch.tensor(self.anchor_grid[i].tolist()).float()  # wh
                 z.append(y.view(bs, -1, self.no))
 
         return x if self.training else (torch.cat(z, 1), x)
